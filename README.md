0f337c16668dc22a12048008dfcdd9fc31481aa7
Sreecharan <sr33ch4@yahoo.com>
2020-07-15 21:28:29 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,54 @@
+<p align="center">
+  <img alt="Icon" src="https://github.com/5R33CH4/Elements/screenshots/elements/icon.png">
+  <h1 align="center">Elements</h1>
+  <p align="center">A Periodic Table Viewer App For Desktop<br><br>
+    <a href="http://forthebadge.com"><img alt="forthebadge" src="http://forthebadge.com/images/badges/built-with-love.svg"></a>
+  <a href="http://forthebadge.com"><img alt="forthebadge" src="http://forthebadge.com/images/badges/uses-js.svg"></a>
+  </p>
+</p>
+
+<br><br>
+
+## 📝 Source Code
+
+In order to use this source code you need to have Node.js, Git and Bower installed globally. After that you have to follow the steps below.
+
+```bash
+# Clone this repository
+git clone https://github.com/5R33CH4/Elements
+# Go into the repository
+cd Elements
+# Install npm dependencies
+npm install
+# Install bower dependencies
+bower install
+# Run the app in test mode
+npm start
+# build application for release
+npm run-script dist
+```
+
+
+## 🖼 Preview
+
+![screenshot 1](https://github.com/5R33CH4/Elements/screenshots/elements/screenshot-1.png)
+![screenshot 2](https://github.com/5R33CH4/Elements/screenshots/elements/screenshot-2.png)
+![screenshot 3](https://github.com/5R33CH4/Elements/screenshots/elements/screenshot-3.png)
+
+## 💾 Download
+
+<table align="center">
+  <tr>
+    <th>Platform</th>
+    <th>Version</th>
+    <th>Download</td>
+  </tr>
+  <tr>
+    <td>Windows</td>
+    <td>1.1.6</td>
+    <td><a href="https://github.com/5R33CH4/Elements/releases/download/v1.1.6/Elements.exe">Download</a></td>
+  </tr>
+</table>
+
+## 📖 License
+[![forthebadge](http://forthebadge.com/images/badges/cc-0.svg)](https://creativecommons.org/publicdomain/zero/1.0/)
---
+++ main.js
@@ -0,0 +1,164 @@
+const electron = require('electron');
+const debounce = require('lodash.debounce')
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const globalShortcut = electron.globalShortcut;
+const ipcMain = electron.ipcMain;
+
+const Menu = electron.Menu;
+
+const path = require('path');
+
+let window = null;
+
+function createWindow() {
+  window = new BrowserWindow({
+    width: 800,
+    height: 545,
+    backgroundColor: '#fff',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  window.loadURL(path.join('file://', __dirname, 'index.html'));
+
+  function forceRedraw() {
+    window.webContents.executeJavaScript(
+      `document.getElementById('periodic-table').style.display = 'none';`);
+
+    setTimeout(() => {
+      window.webContents.executeJavaScript(
+        `document.getElementById('periodic-table').style.display = '';
+        document.getElementsByTagName('body')[0].style.filter = '';`);
+    }, 500);
+  }
+
+  let isRedrawApplied = true;
+  const debouncedRedraw = debounce(() => {
+    isRedrawApplied = true;
+    forceRedraw();
+  }, 200);
+
+  window.on('maximize', () => {
+    isRedrawApplied = false;
+  });
+
+  window.on('move', () => {
+    if (!window.isMaximized() && !isRedrawApplied) {
+      window.webContents.executeJavaScript(
+        `document.getElementsByTagName('body')[0].style.filter = 'blur(5px)';`);
+      debouncedRedraw();
+    }
+  });
+
+  window.on('closed', () => {
+    window = null;
+  });
+
+  const template =
+    [
+      {
+        label: 'Edit',
+        submenu: [
+          { role: 'undo' },
+          { role: 'redo' },
+          { type: 'separator' },
+          { role: 'cut' },
+          { role: 'copy' },
+          { role: 'paste' },
+          { role: 'pasteandmatchstyle' },
+          { role: 'delete' },
+          { role: 'selectall' }
+        ]
+      },
+      {
+        label: 'View',
+        submenu: [
+          { role: 'reload' },
+          { role: 'forcereload' },
+          { role: 'toggledevtools' },
+          { type: 'separator' },
+          { role: 'resetzoom' },
+          { role: 'zoomin' },
+          { role: 'zoomout' },
+          { type: 'separator' },
+          { role: 'togglefullscreen' }
+        ]
+      },
+      {
+        role: 'window',
+        submenu: [
+          { role: 'minimize' },
+          { role: 'close' }
+        ]
+      },
+      {
+        role: 'help',
+        submenu: [
+          {
+            label: 'Learn More',
+            click() { require('electron').shell.openExternal('https://github.com/5R33CH4/Elements') }
+          }
+        ]
+      }
+    ]
+
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services', submenu: [] },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    })
+
+    // Edit menu
+    template[1].submenu.push(
+      { type: 'separator' },
+      {
+        label: 'Speech',
+        submenu: [
+          { role: 'startspeaking' },
+          { role: 'stopspeaking' }
+        ]
+      }
+    )
+
+    // Window menu
+    template[3].submenu = [
+      { role: 'close' },
+      { role: 'minimize' },
+      { role: 'zoom' },
+      { type: 'separator' },
+      { role: 'front' }
+    ]
+  }
+
+  let menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('activate', function () {
+  if (window === null) {
+    createWindow()
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
